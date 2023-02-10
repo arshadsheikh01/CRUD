6ed7f729f38b12f56318a0e1dd229707ea39d7d9
arshadsheikh01 <arshadsheikh9881@gmail.com>
2023-02-10 14:48:16 +0530

    Crid Application

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,38 +7,68 @@ const fnameControl = document.getElementById("fname");
 const lnameControl = document.getElementById("lname");
 const emailControl = document.getElementById("email");
 const contactControl = document.getElementById("contact");
+const updateBtn = document.getElementById("updateBtn");
 const stdContainer = document.getElementById("stdContainer");
-let stdArray = [
-    {fname: 'Arshad ', 
-    lname: 'sheikh', 
-    email: 'arshadsheikh9881@gmail.com', 
-    contact: '9970687501'
-},
-//     {fname: 'sohel', 
-//     lname: 'khan', 
-//     email: 'sohekhan@gmail.com', 
-//     contact: '9325927860'
-// },
-    // {fname: 'Faizan', 
-    // lname: 'Sheikh', 
-    // email: 'faizansheikh321@gmail.com', 
-    // contact: 7720033008},
-];
 
-let result = '';
-stdArray.forEach((std, i) =>{
+let stdArray = [
+    
+]
+const uuid = () => {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (character) => {
+        const random = (Math.random() * 16) | 0;
+        const value = character === "x" ? random : (random & 0x3) | 0x8;
+
+        return value.toString(16);
+    });
+};
+
+const onEdit = (ele) =>{
+    // let getId = ele.getAttribute("data-id");
+    let getId = ele.dataset.id
+    cl(getId);
+    localStorage.setItem("setEditId", getId);
+    let getObj = stdArray.find(obj => obj.id === getId);
+    cl(getObj);
+    fnameControl.value = getObj.fname;
+    lnameControl.value = getObj.lname;
+    emailControl.value = getObj.email;
+    contactControl.value = getObj.contact;
+    submitBtn.classList.add('d-none');
+    updateBtn.classList.remove('d-none');
+}
+const onDelete = (ele) =>{
+    cl('Deleted')
+    let deleteId = ele.dataset.id
+    cl(deleteId)
+    stdArray = stdArray.filter(obj => obj.id !== deleteId);
+    setstdArray();
+    cl(ele.parentElement.parentElement);
+    ele.parentElement.parentElement.remove()
+}
+const tempalating = () =>{
+    let result = '';
+    stdArray.forEach((std, i) =>{
     result += `
                 <tr>
-                    <td>${i +1}</td>
+                    <td>${i + 1}</td>
                     <td>${std.fname}</td>
                     <td>${std.lname}</td>
                     <td>${std.email}</td>
                     <td>${std.contact}</td>
+
+                    <td><button class = "btn btn-primary" data-id="${std.id}" onclick="onEdit(this)">Edit</button></td>
+                    <td><button class = "btn btn-danger" data-id="${std.id}" onclick="onDelete(this)">Delete</button></td>
+
                 </tr>
     
     `
 })
 stdContainer.innerHTML = result;
+}
+if(localStorage.getItem("setStdInfo")){
+    stdArray = JSON.parse(localStorage.getItem("setStdInfo"))
+    tempalating(stdArray);
+}
 const onStdSubmit = (eve) =>{
     eve.preventDefault();
     let obj ={
@@ -46,10 +76,34 @@ const onStdSubmit = (eve) =>{
         lname : lnameControl.value,
         email : emailControl.value,
         contact : contactControl.value,
+        id : uuid()
     }
-    stdArray.push(obj)
-    cl(stdArray)
+    stdArray.push(obj);
+    cl(stdArray);
+    setstdArray();
     stdForm.reset();
 }
+const onStdUpdate = (eve) =>{
+    let getUpdatedId = localStorage.getItem("setEditId");
+    cl(getUpdatedId);
+    stdArray.forEach(obj =>{
+        if(obj.id === getUpdatedId){
+            obj.fname = fnameControl.value;
+            obj.lname = lnameControl.value;
+            obj.email = emailControl.value;
+            obj.contact = contactControl.value;
+        }
+    })
+    setstdArray();
+    tempalating(stdArray);
+    submitBtn.classList.remove('d-none');
+    updateBtn.classList.add('d-none');
+    stdForm.reset()
+}
+function setstdArray(){
+    localStorage.setItem("setStdInfo", JSON.stringify(stdArray));
+}
 
+updateBtn.addEventListener("click", onStdUpdate);
 stdForm.addEventListener("submit", onStdSubmit);
+
